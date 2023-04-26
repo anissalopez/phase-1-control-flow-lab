64c0924e1751d27516af61dc5be1b6154b5c1644
anissalopez <anissa965@gmail.com>
2023-04-25 20:54:21 -0400

    if/else

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+    if (ride <= 400){
+      return 'This one is on me!'
+    }
+    else if(ride > 400 && ride <= 2000){
+      return 'That will be twenty bucks.'
+    }
+    else if (ride > 2000 && ride <= 2500) {
+      return 'I will gladly take your thirty bucks.'
+    }
+    else{
+      return 'No can do.'
+    }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : 'No go.'
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip){
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response;
+
+
   // Write your code here!
 }
